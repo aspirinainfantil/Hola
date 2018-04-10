3aff004750ebf77d44197990cdb662fc8991335e
profesor <profesor@PCProfesor>
2018-04-10 08:46:14 +0200

    commit de fatima

--- ProyectoHola/src/Hola.java
+++ ProyectoHola/src/Hola.java
@@ -3,8 +3,8 @@ public class Hola {
 
 	public static void main(String[] args) {
 		
-		//Cambio realizado por nacho
-		System.out.println("Hola mundo Nacho");
+		//Cambio realizado por fatima
+		System.out.println("Hola mundo Nacho");  // otro
 		System.out.println("Venga adiós");
 		System.out.println("Xaooooooooooooo");
 
